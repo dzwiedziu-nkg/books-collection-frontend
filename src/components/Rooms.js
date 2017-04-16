06c433c3a5be6280cf2a6a74249e443f4fd1d9b7
nkg <nkg753@gmail.com>
2017-04-16 11:45:07 +0200

    Rooms in tiles

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -10,6 +10,36 @@ class Rooms extends React.Component {
   render() {
     const { isLoading, data } = this.props;
 
+    const squareStyle = {
+      float: 'left',
+      position: 'relative',
+      width: '100%',
+      paddingBottom : '100%',
+      overflow: 'hidden',
+      backgroundColor: 'silver'
+    };
+
+    const contentStyle = {
+      position: 'absolute',
+      height: '80%',
+      width: '90%',
+      margin: '10% 5%'
+    };
+
+    const tableStyle = {
+      display: 'table',
+      height: '100%',
+      width: '100%'
+    };
+
+    const tableCellStyle = {
+      display: 'table-cell',
+      verticalAlign: 'middle',
+      textAlign: 'center',
+      height: '100%',
+      width: '100%'
+    };
+
     if (isLoading) {
       return <div>
         <p>loading...</p>
@@ -17,11 +47,23 @@ class Rooms extends React.Component {
     } else {
       return (
         <div>
-          {data.map((item, index) => {
-            return (<div key={index}>
-              {item.name}
-            </div>);
-          })}
+          <div className="row">
+            {data.map((item, index) => {
+              return (
+                <div key={index} className="col-xs-6">
+                  <div style={squareStyle}>
+                    <div style={contentStyle}>
+                      <div style={tableStyle}>
+                        <div style={tableCellStyle}>
+                          <h2>{item.name}</h2>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       );
     }
